53fcb1b2cc711d538384f5cd04594196e5511ea2
Ma'ayan Armony <maayan.armony@kcl.ac.uk>
2022-10-12 10:46:22 +0100

    Change Syntax in models.py

--- things/models.py
+++ things/models.py
@@ -3,11 +3,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Thing(models.Model):
-	name = models.CharField(max_length=30,
-						unique=True,
-						blank=False)
-	description = models.CharField(unique=False,
-							blank=True,
-							max_length=120)
-	quantity = models.IntegerField(unique=False,
-							validators=[MinValueValidator(0), MaxValueValidator(100)])
+	name = models.CharField(max_length=30, unique=True, blank=False)
+	description = models.CharField(unique=False, blank=True, max_length=120)
+	quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
